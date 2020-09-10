b0e9700a66442f67d232f22b02d80ee479f02049
FacundoOs <osores.facundo@gmail.com>
2020-09-10 17:23:01 +0200

    creating a spec to test 'user can send a message', final test passed

---
+++ spec/feature/user_can_send_a_message_spec.rb
@@ -0,0 +1,25 @@
+feature 'User can send a message' do
+  let(:user) {create(:user) }
+
+  context 'successfully' do
+    before do
+      visit root_path
+      click_on "Login"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Log in"
+      click_on "Inbox"
+      click_on "Compose"
+      click_on user.name
+      fill_in "Subject", with: 'text'
+      fill_in "conversation_body", with: 'hey'
+      click_on "Send Message"
+    end
+
+
+    it 'succesfully went into inbox' do
+        expect(page).to have_content 'Your message was successfully sent!'
+    end
+  end
+end
+
